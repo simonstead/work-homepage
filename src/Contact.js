e4b99b57b981839e262421c88f7d6bef6cde33ba
Simon Stead <simonstead@me.com>
2018-04-23 14:50:00 +0100

    add portfolio section and refactor some components

---
+++ src/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Contact = () => (
+  <div id="contact">
+    <h1 className="SectionTitle">contact</h1>
+    <div className="SectionContent">
+      <ul className="ContactList">
+        <li>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/simonstead"
+          >
+            @simonstead
+          </a>
+        </li>
+        <li>+447455174786</li>
+      </ul>
+    </div>
+  </div>
+);
+export default Contact;
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Header = () => (
+  <div className="Header">
+    <h1 className="Logo">S</h1>
+    <nav>
+      <ul className="Nav">
+        <li>
+          <a href="#work">work</a>
+        </li>
+        <li>
+          <a href="#portfolio">portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">contact</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
+
+export default Header;
