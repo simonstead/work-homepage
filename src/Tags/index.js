61584e54a7b3dd6349d0120301c30e2833470ddf
Simon Stead <simonstead@me.com>
2018-10-27 12:58:09 +0100

    add tags

--- src/Tags/index.js
+++ src/Tags/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./styles.css";
 
-const Buzzwords = ['react', 'redux', "docker", 'kubernetes', 'nodejs', 'python', 'golang','C#', 'TDD', 'XP', '#noestimates']
+const Buzzwords = ['react', 'redux', "docker", 'kubernetes', 'nodejs', 'python', 'golang','C#', 'TDD', 'XP', '#noestimates', 'lean', 'agile']
 
 export const Tags = ({tags = Buzzwords}) => (
   <div className={styles.Tags}>
